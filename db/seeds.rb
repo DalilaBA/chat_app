d72687671722bbe3c096bb95f9f9b83bb0c7f392
Dalila <babaameur.dalila@gmail.com>
2021-09-08 15:59:39 +0100

    added the users ans messages db liks and displayed it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "dali", password: "123")
+User.create(username: "ami", password: "123")
+User.create(username: "nani", password: "123")
+User.create(username: "kaki", password: "123")
+User.create(username: "sali", password: "123")
+User.create(username: "bali", password: "123")
+
+Message.create(body: "hello", user_id: 1)
+Message.create(body: "hey everyone", user_id: 4)
+Message.create(body: "I'm nw here hello", user_id: 3)
+Message.create(body: "Welcome", user_id: 5)
+Message.create(body: "Hey hey hey :D", user_id: 2)
+
