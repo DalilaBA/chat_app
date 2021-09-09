9efb042b6d0ee57814b95eb746d3cdfff5439c0a
Dalila <babaameur.dalila@gmail.com>
2021-09-09 10:43:08 +0100

    Restrict views from controller layer

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomController < ApplicationController
+  before_action :require_user #this simple line will restrict the no logged in users 
   def index
     @messages = Message.all
   end
