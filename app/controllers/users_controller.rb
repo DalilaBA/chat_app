7d2a5cb5d08e9aef68a77394ffd8c2161d157248
Dalila <babaameur.dalila@gmail.com>
2021-11-06 15:54:59 +0100

    something went off

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+# before_action :reqire_user, except: [:edit, :update, :destroy]
+before_action :require_own_user, only: [:edit, :update, :destroy]
+
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    # @user.avatar.attach(params[:avatar])
+    # byebug
+    # @articles = @user.articles
+    # @articles = @user.articles.paginate(page: params[:page], per_page: 3)
+
+    
+  end
+  def index
+    # @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end
+
+  def create
+    # byebug
+    @user = User.new(user_params)
+    # @user.avatar.attach(params[:avatar])
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] ="Welcom #{@user.username} to the Developement blog, you have successfully sign up"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Your account and all associated articles has been successfully deleted"
+    redirect_to articles_path
+  end
+
+
+  def edit
+  end
+
+  def update
+    # @user.avatar.attach(params[:avatar])
+    # byebug
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfuly updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+  def require_own_user
+    if current_user !=@user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own profile"
+      redirect_to @user
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to:'messages#create'
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   mount ActionCable.server, at: '/cable' #communication via that route will be possible in my app
 end
